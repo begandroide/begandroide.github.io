d9ca11331fa4a838fe0d807027f9b815f7d942ad
Benjamín Gautier <benjamin.gautier.14@sansano.usm.cl>
2021-09-04 21:44:21 -0400

    create footer and header component

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,16 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
-import IconThemeLightDark from '~icons/mdi/theme-light-dark'
-import { toggleDark, isDark } from './logic/dark'
+import Header from './components/Header.vue'
+
+import { isDark } from './logic/dark'
 import darkLogo from './assets/bg_dark.svg'
 import lightLogo from './assets/bg_light.svg'
 </script>
 
 <template>
-  <div class="h-full flex flex-col place-items-stretch">
-    <div class="flex-none min-h-10 border-b-1 pb-2 border-b-gray-300 flex flex-row justify-between items-center dark:bg-gray-900">
-      <img id="bg-mini-logo" alt="bg mini logo" v-bind:src="isDark ? darkLogo : lightLogo">
-      <button class="inline-flex p-2 text-xl" @click="toggleDark()">
-        <icon-theme-light-dark style="font-size: 2em;" class="cursor-pointer p-1 btn-ld text-black dark:text-white "/>
-      </button>
-
-    </div>
+  <div id="sup" class="h-full flex flex-col place-items-stretch">
+    <Header />
     <div class="flex-auto dark:bg-gray-900 flex flex-col items-center">
       <img id="bg-logo" alt="bg logo" v-bind:src="isDark ? darkLogo : lightLogo">
       <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
@@ -34,7 +29,4 @@ import lightLogo from './assets/bg_light.svg'
   height: 16em;
 }
 
-.btn-ld:hover {
-  cursor: pointer;
-}
 </style>
