f5f0fb177677bdd613a8e92915c7b6b94091ae70
Benjamín Gautier <benjamin.gautier.14@sansano.usm.cl>
2022-07-10 12:34:13 -0400

    enable markdown and header configs to vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
-import path from 'path'
+import path, { resolve } from 'path'
 import WindiCSS from 'vite-plugin-windicss'
 import Icons from 'unplugin-icons/vite'
+import Markdown, { code, link, meta } from 'vite-plugin-md'
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      include: [/\.vue$/, /\.md$/], // <--
+    }),
     // https://github.com/antfu/unplugin-icons
     Icons({compiler: 'vue3'}),
     // https://github.com/antfu/vite-plugin-windicss
@@ -23,6 +27,43 @@ export default defineConfig({
       // you need to set i18n resource including paths !
       compositionOnly: true,
       include: [path.resolve(__dirname, 'locales/**')],
+    }),
+    Markdown({
+      // headEnabled: true,
+      builders: [
+        meta({
+          routeProps: ['layout', 'requireAuth'],
+          defaults: {
+            requireAuth: () => false,
+          },
+        }),
+        link(),
+        code({
+          theme: 'base',
+          layoutStructure: 'tabular',
+        }),
+      ],
+    }),
+    Pages({
+      dirs: [
+        // issue #68
+        { dir: resolve(__dirname, './src/pages'), baseRoute: '' },
+      ],
+      extensions: ['vue', 'md'],
+      extendRoute(route: any) {
+        if (route.name === 'about')
+          route.props = (route: any) => ({ query: route.query.q })
+
+        if (route.name === 'components') {
+          return {
+            ...route,
+            beforeEnter: (route: any) => {
+              // eslint-disable-next-line no-console
+              console.log(route)
+            },
+          }
+        }
+      },
     }),
   ]
 })
