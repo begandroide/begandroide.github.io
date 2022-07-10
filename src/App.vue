f0cf6aa7f9924257c5de93ed3f8874f3c981b081
Benjamín Gautier <benjamin.gautier.14@sansano.usm.cl>
2022-07-10 12:34:28 -0400

    settings

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
+
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Home from './components/Home.vue'
 import Header from './components/Header.vue'
+
 </script>
 
 <template>
-  <div id="sup" class="h-full flex flex-col place-items-stretch">
+  <div id="sup" class="h-full flex flex-col place-items-stretch relative">
     <Header />
-    <Home msg="Hello Vue 3 + TypeScript + Vite" />
+    <!-- <Home msg="Hello Vue 3 + TypeScript + Vite" /> -->
+    <div class="m-24">
+      <router-view />
+    </div>
+    <!-- <Home2></Home2> -->
   </div>
 </template>
 
